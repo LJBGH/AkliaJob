df7aa09b75e0ff5fc996223ec96089259e4e491d
LJBGH <1983810978@qq.com>
2021-10-19 19:36:14 +0800

    [Modified]

--- src/AkliaJob.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AkliaJob.Services
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/AkliaJob.Services/Schedule/IScheduleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AkliaJob.Services.Schedule
+{
+    public interface IScheduleService
+    {       
+
+    }
+}
