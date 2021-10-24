dfbd0ae0650f22ca7c7294e3de969a68eb2ebb76
LJBGH <1983810978@qq.com>
2021-10-24 18:28:21 +0800

    [Modified]

--- src/AkliaJob.App/Program.cs
+++ src/AkliaJob.App/Program.cs
@@ -1,16 +1,16 @@
-﻿using AkliaJob.App.Services;
+﻿using AkliaJob.App.Extensions;
+using AkliaJob.App.Services;
+using AkliaJob.App.StartupModels;
+using AkliaJob.Shared;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using AkliaJob.Shared;
-using Microsoft.Extensions.Logging;
-using AkliaJob.App.StartupModels;
-using Serilog;
-using AkliaJob.App.Extensions;
 
 namespace AkliaJob.App
 {
@@ -81,11 +81,11 @@ namespace AkliaJob.App
 
                     if (isConn)
                     {
-                        await Heartbeat();
-                        await StartReceiving(client, testService, logger);
+                        StartReceiving(client, testService, logger);
+                        Heartbeat();
                     }
                 })
-     
+
                 .UseSerilog()
                 .AddSerilog();
 
@@ -94,22 +94,22 @@ namespace AkliaJob.App
         /// WebSocket连接开启
         /// </summary>
         /// <returns></returns>
-        static async Task<bool> TryConnectWebSocket() 
+        static async Task<bool> TryConnectWebSocket()
         {
             _socketLoopTokenSource = new CancellationTokenSource();
             _sendLoopTokenSource = new CancellationTokenSource();
-            if (client == null) 
+            if (client == null)
             {
                 client = new ClientWebSocket();
             }
-            if (client.State == WebSocketState.Open) 
+            if (client.State == WebSocketState.Open)
             {
                 return true;
             }
             try
             {
                 client.ConnectAsync(new Uri("ws://localhost:5000/ws"), CancellationToken.None).Wait();
-                byte[] resultbuffer = System.Text.Encoding.Default.GetBytes($"客户端连接成功");
+                byte[] resultbuffer = System.Text.Encoding.UTF8.GetBytes($"客户端连接成功");
                 await client.SendAsync(new ArraySegment<byte>(resultbuffer, 0, resultbuffer.Length),
                           WebSocketMessageType.Text,
                           true,
@@ -131,31 +131,34 @@ namespace AkliaJob.App
         /// 心跳检测
         /// </summary>
         /// <returns></returns>
-        static async Task Heartbeat() 
+        static void Heartbeat()
         {
-            var token = _sendLoopTokenSource.Token;
-            var data = Encoding.UTF8.GetBytes("sent ping to server");
-            var i = 0;
-            while (i < 10 && !token.IsCancellationRequested) 
+            Task.Run(async () =>
             {
-                await Task.Delay(1000, token);
-                if (client?.State == WebSocketState.Open) 
+                var token = _sendLoopTokenSource.Token;
+                var data = Encoding.UTF8.GetBytes("sent ping to server");
+                var i = 0;
+                while (i < 10 && !token.IsCancellationRequested)
                 {
-                    try
+                    await Task.Delay(1000, token);
+                    if (client?.State == WebSocketState.Open)
                     {
-                        if (!token.IsCancellationRequested) 
+                        try
                         {
-                            await client.SendAsync(new ArraySegment<byte>(data), WebSocketMessageType.Text, true, CancellationToken.None);
+                            if (!token.IsCancellationRequested)
+                            {
+                                await client.SendAsync(new ArraySegment<byte>(data), WebSocketMessageType.Text, true, CancellationToken.None);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
                         }
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
-                }
 
-                i++;
-            }
+                    i++;
+                }
+            });
         }
 
 
@@ -164,39 +167,46 @@ namespace AkliaJob.App
         /// 监听接收消息
         /// </summary>
         /// <param name="client"></param>
-        static async Task StartReceiving(ClientWebSocket client, ITestService testService, ILogger<Program> logger)
+        static void StartReceiving(ClientWebSocket client, ITestService testService, ILogger<Program> logger)
         {
-            try
+            var cancellationToken = _socketLoopTokenSource.Token;
+            Task.Run(async () =>
             {
-                var cancellationToken = _socketLoopTokenSource.Token;
-                WebSocketReceiveResult result = null;
-                var buffer = new byte[4096];
-                while (client.State != WebSocketState.Closed && !cancellationToken.IsCancellationRequested)
+                try
                 {
-                    result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-                    if (client.State == WebSocketState.CloseReceived && result.MessageType == WebSocketMessageType.Close) 
+                    WebSocketReceiveResult result = null;
+                    var buffer = new byte[4096];
+                    while (client.State != WebSocketState.Closed && !cancellationToken.IsCancellationRequested)
                     {
-                        _sendLoopTokenSource.Cancel();
-                        await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "关闭客户端", CancellationToken.None);
-                    }
+                        result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
-                    if (client.State == WebSocketState.Open)
-                    {
-                        //文本类型消息
-                        if (result.MessageType == WebSocketMessageType.Text)
+                        if (!cancellationToken.IsCancellationRequested)
                         {
-                            //await testService.TestAsync();
-                            string msg = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                            logger.LogInformation($"{msg}");
+                            if (client.State == WebSocketState.CloseReceived && result.MessageType == WebSocketMessageType.Close)
+                            {
+                                _sendLoopTokenSource.Cancel();
+                                await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "关闭客户端", CancellationToken.None);
+                            }
+
+                            if (client.State == WebSocketState.Open)
+                            {
+                                //文本类型消息
+                                if (result.MessageType == WebSocketMessageType.Text)
+                                {
+                                    //await testService.TestAsync();
+                                    string msg = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                                    logger.LogInformation($"{msg}");
+                                }
+                            }
                         }
                     }
-                } 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            });
+
         }
     }
 }
